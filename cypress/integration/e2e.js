29b77ed1e9b571449f89781243c5fa9e2d2dbe80
Camden Mecklem <c@camdenmecklem.com>
2020-12-10 23:22:34 -0500

    added cypress test for specific pokemon page

--- cypress/integration/e2e.js
+++ cypress/integration/e2e.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to a specific pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').click()
+    cy.contains('lightning rod')
+  })
 })
